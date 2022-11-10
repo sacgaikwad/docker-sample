0ea987e191f81723673fcb0830176c1c8f55acda
sachin <>
2022-11-10 19:02:54 +0530

    Linq test

---
+++ docker.example/docker.unittest/LinqTest.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace docker.unittest
+{
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+    [TestClass]
+    public class LinqTest
+    {
+
+        [TestMethod]
+        public void Sample()
+        {
+
+            List<Employee> employeeList = new List<Employee>()
+            {
+               new Employee() { Id = 1, Name = "Sunny", Department = "Technical" },
+               new Employee() { Id=2, Name="Pinki", Department ="HR"},
+               new Employee() { Id=3, Name="Tensy", Department ="Finance"},
+               new Employee() { Id=4, Name="Bobby", Department ="Technical"},
+               new Employee() { Id=5, Name="Sweety", Department ="HR"}
+            };
+
+            var result = employeeList.First();
+            var resultHr = employeeList.First(e => e.Department == "HR");
+
+            var resultHrDefault = employeeList.FirstOrDefault(e => e.Id == 8);
+
+            try
+            {
+                List<Employee> employeeListNew = new List<Employee>();
+                var resultOne = employeeListNew.First();
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            try
+            {
+                List<Employee> employeeListNew = new List<Employee>();
+                var resultOne = employeeListNew.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            try
+            {
+                var resultSingle = employeeList.Single(e => e.Department == "HR");
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            try
+            {
+                var resultSingle = employeeList.SingleOrDefault(e => e.Department == "HR");
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+            List<int> intLst = new List<int>();
+            intLst.Add(1);
+            intLst.Add(2);
+
+            var rDefault = intLst.FirstOrDefault(x => x.Equals(0));
+
+            try
+            {
+                var rFirst = intLst.First(x => x.Equals(0));
+            }
+            catch (Exception)
+            {
+
+
+            }
+
+
+        }
+    }
+}
---
+++ docker.example/docker.unittest/ReadMe.txt
@@ -0,0 +1,38 @@
+﻿First()
+It returns the first element of a sequence.
+It throws an error when there is no element in the result, or source is null.
+We should use it if more than one element is expected and you want only the first element.
+
+FirstOrDefault()
+It returns the first element of a sequence, or a default value if no element is found.
+It throws an error only if the source is null.
+We should use it if more than one element is expected and you want only the first element. It's also good if the result is empty.
+
+Single()
+It returns the only item of a sequence.
+This will throw an exception if the result contains 0 or more than 1 elements.
+We should use it, when we know that exactly one element is expected but neither 0 nor 2 or more.
+
+SingleOrDefault()
+It returns single specific element, and if the element is not found, it returns the default value of it.
+This will throw an exception if the result contains 2 or more elements.
+We should use it when we know that 0 or 1 element is expected as result.
+
+
+If your result set returns 0 records:
+
+SingleOrDefault returns the default value for the type (e.g. default for int is 0)
+FirstOrDefault returns the default value for the type
+If you result set returns 1 record:
+
+SingleOrDefault returns that record
+FirstOrDefault returns that record
+If your result set returns many records:
+
+SingleOrDefault throws an exception
+FirstOrDefault returns the first record
+Conclusion:
+
+If you want an exception to be thrown if the result set contains many records, use SingleOrDefault.
+
+If you always want 1 record no matter what the result set contains, use FirstOrDefault
